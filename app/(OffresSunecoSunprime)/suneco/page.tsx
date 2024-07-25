b76b989f67d747e093608d181ff407ae01c5d304
Max17800 <156446025+Max17800@users.noreply.github.com>
2024-07-25 23:39:58 +0200

    Update page.tsx

--- app/(OffresSunecoSunprime)/suneco/page.tsx
+++ app/(OffresSunecoSunprime)/suneco/page.tsx
@@ -36,7 +36,7 @@ export default function Suneco() {
           <li className="flex items-center gap-5">
             <Check color="#00A06B" size={20} strokeWidth={3} />
             <span className="max-w-[80%]">
-              Installation de qualité par technicien RGE Sunline
+              Installation de qualité par nos techniciens RGE Sunline
             </span>
           </li>
         </>
