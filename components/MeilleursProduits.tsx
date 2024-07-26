5c9e209db07ca09a5cb292dd0b82479b8c3f12f2
Max17800 <156446025+Max17800@users.noreply.github.com>
2024-07-26 15:26:01 +0200

    Update MeilleursProduits.tsx

--- components/MeilleursProduits.tsx
+++ components/MeilleursProduits.tsx
@@ -62,7 +62,7 @@ export default function MeilleursProduits() {
           />
           <div className="bg-[#F5F6F7] text-center lg:text-start px-7 py-8 sm:flex sm:flex-col sm:items-center lg:items-start lg:w-[550px] xl:w-[600px]">
             <h3 className="font-bold text-xl mb-8 sm:w-[500px] md:w-[650px] lg:w-[450px] xl:w-[500px]">
-              Le micro-onduleur le plus puissant d'Enphase le leader mondial, l&apos;IQ8P, avec 475 VA de puissance, 
+              Le micro-onduleur le plus puissant d'Enphase le leader mondial, IQ8P, avec 475 VA de puissance, 
               assure une connexion optimale de vos panneaux solaires au réseau national.
             </h3>
             <p className="mb-8 sm:w-[500px] md:w-[650px] lg:w-[450px] xl:w-[500px]">
@@ -89,9 +89,9 @@ export default function MeilleursProduits() {
               APsystems dévoile la série DS3, le micro-onduleur DUO le plus puissant du marché, alliant puissance et rentabilité.
             </h3>
             <p className="mb-8 sm:w-[500px] md:w-[650px] lg:w-[450px] xl:w-[500px]">
-              Grâce à plusieurs offres de puissance pouvant aller jusqu&apos;à 960 VA.
+              Grâce à plusieurs offres de puissance pouvant aller jusquà 960 VA.
               Avec ses 2 MPPT indépendants ainsi qu’une communication Zigbee cryptée, 
-              les DS3 se dotent d&apos;une toute nouvelle architecture et sont toujours compatibles avec les micro-onduleurs QS1.
+              les DS3 se dotent dune toute nouvelle architecture et sont toujours compatibles avec les micro-onduleurs QS1.
             </p>
             <div className="w-6 h-1 bg-[#2BA0B5] mx-auto lg:mx-0 mb-8"></div>
             <span className="text-lg font-bold">20 ans</span>
