5d507fce5bb8c37bcc6a8446499df931cc9ae635
Max17800 <156446025+Max17800@users.noreply.github.com>
2024-07-27 13:12:46 +0200

    Update BoxOffre.tsx

--- components/Home/BoxOffre.tsx
+++ components/Home/BoxOffre.tsx
@@ -34,7 +34,7 @@ export default function BoxOffre({
           <p className="mt-5">{desc}</p>
         </div>
         <Image
-          src={"/assets/test1.png"}
+          src={"/assets/SunPower_P7_Tier1_6-400x397.png.png"}
           width={477}
           height={525}
           alt="image offre"
