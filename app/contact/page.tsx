cdaa6ed84528ca7a9a35d654cf457a1bda1330a5
Max17800 <156446025+Max17800@users.noreply.github.com>
2024-08-12 16:07:56 +0200

    Update page.tsx

--- app/contact/page.tsx
+++ app/contact/page.tsx
@@ -19,8 +19,7 @@ export default function Contact() {
             Nos experts se feront un plaisir de répondre à toutes vos demandes
             et vous fournir tous les renseignements nécessaires.
           </p>
-         <script type="text/javascript" src="https://apps.reonic.de/elements/reonic-loader.js" defer></script>
-         <div data-reonic-type="element" data-product="energyhouse" data-client-id="8802b02f-711e-4b78-ba14-aa0453d6c062"></div>
+       <ContactForm />
         </div>
         <div className="sm:w-[550px] sm:mx-auto xl:mx-0 xl:w-[400px]">
           <Image
