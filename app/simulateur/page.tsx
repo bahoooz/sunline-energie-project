67d6728175dc2b8157c4a1baf7064784a92806ec
Max17800 <156446025+Max17800@users.noreply.github.com>
2024-08-14 16:49:51 +0200

    Update page.tsx

--- app/simulateur/page.tsx
+++ app/simulateur/page.tsx
@@ -6,7 +6,7 @@ import HeaderGaranties from "@/components/Garanties/HeaderGaranties";
 import Partenaires from "@/components/Home/Partenaires";
 import InstallationsSolaires from "@/components/InstallationsSolaires";
 import MeilleursProduits from "@/components/MeilleursProduits";
-import PageSimulateur from "@/components/Simulateur";
+import PageSimulateur from "@/components/Simulateur/PageSimulateur";
 import React from "react";
 
 export default function Garanties() {
