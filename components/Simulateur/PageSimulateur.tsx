14297f48571cd42a738944fcea5a95ca91f28063
Max17800 <156446025+Max17800@users.noreply.github.com>
2024-08-14 20:15:42 +0200

    Update PageSimulateur.tsx

--- components/Simulateur/PageSimulateur.tsx
+++ components/Simulateur/PageSimulateur.tsx
@@ -3,7 +3,7 @@ import React from "react";
 export default function PageSimulateur() {
   return (
      <div className="mt-20" id="page-simulateur">
-      <script type="text/javascript" src="https://apps.reonic.de/elements/reonic-loader.js" defer ></script>
+      <script type="text/javascript" src="https://apps.reonic.de/elements/reonic-loader.js" defer></script>
       <div data-reonic-type="element" data-product="energyhouse" data-client-id="8802b02f-711e-4b78-ba14-aa0453d6c062"></div>
     </div>
   );
