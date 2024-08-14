9f8a3084541074e2d12270234e71c9831c39ad34
Max17800 <156446025+Max17800@users.noreply.github.com>
2024-08-14 17:43:44 +0200

    Update HeaderHome2.tsx

--- components/Home/HeaderHome2.tsx
+++ components/Home/HeaderHome2.tsx
@@ -7,7 +7,7 @@ export default function HeaderHome() {
   return (
     <div>
       <div id="header-home" className="h-[70vh]">
-        <div className="text-center lg:text-start flex flex-col gap-10 pt-24 2xl:pt-32 px-6 lg:px-24 2xl:px-48">
+        <div className="text-center lg:text-start flex flex-col gap-10 pt-18 2xl:pt-18 px-6 lg:px-16 2xl:px-24">
           <h1 className="text-white text-3xl lg:text-5xl font-semibold">
             Votre énergie solaire, <br className="hidden lg:block" /> notre
             engagement durable
