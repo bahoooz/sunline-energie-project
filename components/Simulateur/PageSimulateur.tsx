9108072386d2cf0c42ad917f43cc4f9d5b652823
Max17800 <156446025+Max17800@users.noreply.github.com>
2024-08-14 20:45:01 +0200

    Update PageSimulateur.tsx

--- components/Simulateur/PageSimulateur.tsx
+++ components/Simulateur/PageSimulateur.tsx
@@ -7,7 +7,7 @@ export default function PageSimulateur() {
     const script = document.createElement("script");
     script.src = "https://apps.reonic.de/elements/reonic-loader.js";
     script.type = "text/javascript";
-    script.defer = true;
+    script.defer = false;
     document.body.appendChild(script);
 
     return () => {
