bb72b44785bd99e55e914b6a5da54ea66aa379ed
Max17800 <156446025+Max17800@users.noreply.github.com>
2024-08-14 17:58:16 +0200

    Update HeaderHome2.tsx

--- components/Home/HeaderHome2.tsx
+++ components/Home/HeaderHome2.tsx
@@ -9,8 +9,8 @@ export default function HeaderHome() {
       <div id="header-home" className="h-[50vh]">
         <div className="text-center lg:text-start flex flex-col gap-10 pt-24 2xl:pt-32 px-6 lg:px-24 2xl:px-48">
           <h1 className="text-white text-3xl lg:text-5xl font-semibold">
-            Votre énergie solaire, <br className="hidden lg:block" /> notre
-            engagement durable
+            En quelques clics, <br className="hidden lg:block" /> simulez
+            votre futur installation photovoltaïque
           </h1>
           <div className="flex flex-col items-center gap-5 lg:flex-row lg:items-center">
             <Button
