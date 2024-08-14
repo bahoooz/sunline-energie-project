2c338062e90fd6032a80197d1a7173f669f812fe
Max17800 <156446025+Max17800@users.noreply.github.com>
2024-08-14 20:41:10 +0200

    Update PageSimulateur.tsx

--- components/Simulateur/PageSimulateur.tsx
+++ components/Simulateur/PageSimulateur.tsx
@@ -1,18 +1,25 @@
-import React from "react";
+import React, { useEffect } from "react";
 
 export default function PageSimulateur() {
+  useEffect(() => {
+    const script = document.createElement("script");
+    script.src = "https://apps.reonic.de/elements/reonic-loader.js";
+    script.type = "text/javascript";
+    script.defer = true;
+    document.body.appendChild(script);
+
+    return () => {
+      document.body.removeChild(script); // Nettoyage si le composant est démonté
+    };
+  }, []);
+
   return (
     <div className="mt-20" id="page-simulateur">
-  <script
-    type="text/javascript"
-    src="https://apps.reonic.de/elements/reonic-loader.js"
-    defer
-  ></script>
-  <div
-    data-reonic-type="element"
-    data-product="energyhouse"
-    data-client-id="8802b02f-711e-4b78-ba14-aa0453d6c062"
-  ></div>
-</div>
+      <div
+        data-reonic-type="element"
+        data-product="energyhouse"
+        data-client-id="8802b02f-711e-4b78-ba14-aa0453d6c062"
+      ></div>
+    </div>
   );
 }
