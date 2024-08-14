de8a6de54ede7c8290a02636122bb642a33a2a42
Max17800 <156446025+Max17800@users.noreply.github.com>
2024-08-14 17:44:53 +0200

    Update HeaderHome2.tsx

--- components/Home/HeaderHome2.tsx
+++ components/Home/HeaderHome2.tsx
@@ -21,7 +21,7 @@ export default function HeaderHome() {
               Simulez votre installation Solaire
             </Button>
             <p className="text-white text-sm lg:mt-2">
-              Recevez immédiatement l'étude détaillée
+              Recevez immédiatement votre etude
             </p>
           </div>
         </div>
