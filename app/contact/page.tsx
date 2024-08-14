5ba893a41cdb6346bd6eb4d42866878aab73b80a
Max17800 <156446025+Max17800@users.noreply.github.com>
2024-08-14 20:32:19 +0200

    Update page.tsx

--- app/contact/page.tsx
+++ app/contact/page.tsx
@@ -89,18 +89,6 @@ export default function Contact() {
             </div>
           </div>
         </div>
-            <div className="mt-20" id="page-simulateur">
-  <script
-    type="text/javascript"
-    src="https://apps.reonic.de/elements/reonic-loader.js"
-    defer
-  ></script>
-  <div
-    data-reonic-type="element"
-    data-product="energyhouse"
-    data-client-id="8802b02f-711e-4b78-ba14-aa0453d6c062"
-  ></div>
-</div>
       </div>
       <InstallationsSolaires />
       <FAQ />
