d3a1451d54dc8e2f01cf0b628100ebbd92b36239
Max17800 <156446025+Max17800@users.noreply.github.com>
2024-08-21 09:18:32 +0200

    Update page.tsx

--- app/avis/page.tsx
+++ app/avis/page.tsx
@@ -1,5 +1,4 @@
-"use client";
-
+import Head from 'next/head';
 import HeaderAvis from "@/components/Avis/HeaderAvis";
 import InstallationsSolaires from "@/components/InstallationsSolaires";
 import FAQ from "@/components/FAQ";
@@ -7,67 +6,46 @@ import { Star } from "lucide-react";
 import React from "react";
 
 export default function Avis() {
-  // const [reviews, setReviews] = useState([]);
-
-  // useEffect(() => {
-  //   const getReviews = async () => {
-  //     try {
-  //       const response = await fetch("/api/reviews");
-  //       if (!response.ok) {
-  //         throw new Error("Network response was not ok");
-  //       }
-  //       const data = await response.json();
-  //       setReviews(data);
-  //     } catch (error) {
-  //       console.error("Erreur lors de la récupération des avis :", error);
-  //     }
-  //   };
-
-  //   getReviews();
-  // }, []);
-
   return (
-    <div className="mt-32">
-      <HeaderAvis />
-      <div className="mt-20">
-        <h2 className="text-center font-bold text-2xl lg:text-3xl px-7">
-          Ils ont été installés par Sunline
-        </h2>
-        <div className="flex flex-col items-center mt-12 gap-12">
-          <div className="flex items-end gap-3">
-            <div className="flex">
-              <Star fill="#FEB50B" strokeWidth={0} size={32} />
-              <Star fill="#FEB50B" strokeWidth={0} size={32} />
-              <Star fill="#FEB50B" strokeWidth={0} size={32} />
-              <Star fill="#FEB50B" strokeWidth={0} size={32} />
-              <Star fill="#FEB50B" strokeWidth={0} size={32} />
-            </div>
-            <span className="font-bold text-2xl">5</span>
-          </div>
-          <p className="text-xl lg:text-2xl">
-            {/* {reviews.length} avis Google et Facebook */}
-            11 avis Google et Facebook
-          </p>
-          {/* <div>
-            {reviews.map((review, index) => (
-              <div key={index} className="mt-4">
-                <p className="font-bold">{review.author_name}</p>
-                <p>{review.text}</p>
-                <div className="flex">
-                  {[...Array(review.rating)].map((_, i) => (
-                    <Star key={i} fill="#FEB50B" strokeWidth={0} size={24} />
-                  ))}
-                  {[...Array(5 - review.rating)].map((_, i) => (
-                    <StarOff key={i} size={24} />
-                  ))}
-                </div>
+    <>
+      <Head>
+        <script>
+          {`(function (w,d,s,o,r,js,fjs) {
+            w[r]=w[r]||function() {(w[r].q = w[r].q || []).push(arguments)}
+            w[r]('app', '8Fbj3GBuLv');
+            if(d.getElementById(o)) return;
+            js = d.createElement(s), fjs = d.getElementsByTagName(s)[0];
+            js.id = o; js.src = 'https://embed.trustmary.com/embed.js';
+            js.async = 1; fjs.parentNode.insertBefore(js, fjs);
+          }(window, document, 'script', 'trustmary-embed', 'tmary'));`}
+        </script>
+      </Head>
+      <div className="mt-32">
+        <HeaderAvis />
+        <div className="mt-20">
+          <h2 className="text-center font-bold text-2xl lg:text-3xl px-7">
+            Ils ont été installés par Sunline
+          </h2>
+          <div className="flex flex-col items-center mt-12 gap-12">
+            <div className="flex items-end gap-3">
+              <div className="flex">
+                <Star fill="#FEB50B" strokeWidth={0} size={32} />
+                <Star fill="#FEB50B" strokeWidth={0} size={32} />
+                <Star fill="#FEB50B" strokeWidth={0} size={32} />
+                <Star fill="#FEB50B" strokeWidth={0} size={32} />
+                <Star fill="#FEB50B" strokeWidth={0} size={32} />
               </div>
-            ))}
-          </div> */}
+              <span className="font-bold text-2xl">5</span>
+            </div>
+            <p className="text-xl lg:text-2xl">
+              11 avis Google et Facebook
+            </p>
+            <div data-trustmary-widget="jkrmJBDvb"></div>
+          </div>
         </div>
+        <FAQ />
+        <InstallationsSolaires />
       </div>
-      <FAQ />
-      <InstallationsSolaires />
-    </div>
+    </>
   );
 }
