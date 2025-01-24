51200c05f58e83e6255a81bdae079334573b6028
Max17800 <156446025+Max17800@users.noreply.github.com>
2025-01-24 12:53:13 +0100

    Update Navbar.tsx

--- components/Navbar.tsx
+++ components/Navbar.tsx
@@ -66,7 +66,7 @@ export default function NavbarComponent() {
       <Navbar
         onMenuOpenChange={setIsMenuOpen}
         isMenuOpen={isMenuOpen}
-        className={`h-20 shadow-md bg-white ${isMenuOpen ? "overflow-auto" : ""}`}
+        className="h-20 shadow-md bg-white"
         id="navbar"
       >
         <NavbarContent>
@@ -126,7 +126,9 @@ export default function NavbarComponent() {
             </Link>
           </NavbarItem>
         </NavbarContent>
-        <NavbarMenu className="bg-white text-black flex flex-col justify-center gap-6 top-0 left-0 w-full max-h-[80vh] pb-2 overflow-y-auto">
+        <NavbarMenu
+          className={`bg-white text-black flex flex-col justify-center gap-6 top-0 left-0 w-full max-h-[60vh] pb-2 overflow-y-auto transition-transform duration-300 ${isMenuOpen ? "translate-y-0" : "-translate-y-full"}`}
+        >
           <NavbarMenuItem className="text-xl">
             <Link href={"/offres"} onClick={() => setIsMenuOpen(false)} className="hover:text-[#FBAC18] transition-colors duration-300">
               Solutions photovoltaïques
