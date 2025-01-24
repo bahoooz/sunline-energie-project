bc5cf815f990532d820f028e7357e9d4da3cd614
Max17800 <156446025+Max17800@users.noreply.github.com>
2025-01-24 11:16:27 +0100

    Update BoxOffre.tsx

--- components/Home/BoxOffre.tsx
+++ components/Home/BoxOffre.tsx
@@ -9,8 +9,8 @@ interface BoxOffre {
   desc: string;
   list: ReactElement;
   image: string;
-  option?: HTMLDivElement | null;
-  link?: string;
+  option?: HTMLDivElement | any;
+  link?: any;
 }
 
 export default function BoxOffre({
