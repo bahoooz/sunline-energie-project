213d65b801b10e28283865eeef13c73df18d135f
Max17800 <156446025+Max17800@users.noreply.github.com>
2025-01-24 13:06:52 +0100

    Update Navbar.tsx

--- components/Navbar.tsx
+++ components/Navbar.tsx
@@ -4,15 +4,6 @@
 
 /* eslint-disable prettier/prettier */
 import React, { useState } from "react";
-import {
-  Navbar,
-  NavbarBrand,
-  NavbarContent,
-  NavbarItem,
-  NavbarMenu,
-  NavbarMenuItem,
-  NavbarMenuToggle,
-} from "@nextui-org/navbar";
 import Image from "next/image";
 import Link from "next/link";
 import { Button } from "@nextui-org/button";
@@ -43,7 +34,7 @@ export default function NavbarComponent() {
           </Button>
         </Link>
         <div className="hidden lg:flex gap-8 items-center">
-          <NavbarBrand as={Link} href={"/"}>
+          <Link href={"/"}>
             <Image
               className="w-32"
               src={"/logo_black.png"}
@@ -51,7 +42,7 @@ export default function NavbarComponent() {
               height={83}
               alt="logo"
             />
-          </NavbarBrand>
+          </Link>
           <div className="flex gap-6 font-semibold">
             <Link href="/offres" className="hover:text-white transition-all duration-300">Solutions photovoltaïques</Link>
             <Link href="/garanties" className="hover:text-white transition-all duration-300">Garanties</Link>
@@ -60,11 +51,15 @@ export default function NavbarComponent() {
             <Link href="/contact" className="hover:text-white transition-all duration-300">Nous contacter</Link>
           </div>
         </div>
-        <NavbarMenuToggle
+        <button
           aria-label={isMenuOpen ? "Close menu" : "Open menu"}
-          className="md:hidden"
+          className="md:hidden focus:outline-none"
           onClick={() => setIsMenuOpen(!isMenuOpen)}
-        />
+        >
+          <div className="w-6 h-6 bg-white rounded-md flex items-center justify-center">
+            <span className="block w-4 h-0.5 bg-[#FBAC18] transform transition-transform duration-300"></span>
+          </div>
+        </button>
       </div>
       {isMenuOpen && (
         <div className="bg-white w-full fixed top-16 left-0 z-10 text-black shadow-md">
