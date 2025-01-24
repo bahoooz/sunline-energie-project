9979000bf3087271d2e7abd700f14e471245197c
Max17800 <156446025+Max17800@users.noreply.github.com>
2025-01-24 17:19:07 +0100

    Update page.tsx

--- app/mentions-legales/page.tsx
+++ app/mentions-legales/page.tsx
@@ -2,7 +2,7 @@ import React from "react";
 
 export default function MentionsLegales() {
   return (
-    <div className="pt-20 p-8 bg-gray-100 text-gray-900">
+    <div className="pt-32 p-8 bg-gray-100 text-gray-900">
       <div className="max-w-4xl mx-auto">
         <h1 className="text-2xl font-bold mb-4">Mentions Légales</h1>
 
