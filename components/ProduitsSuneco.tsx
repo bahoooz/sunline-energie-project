9c04d150f2a4012e7b179c7f73007de1aeb38a69
Max17800 <156446025+Max17800@users.noreply.github.com>
2025-01-29 14:34:23 +0100

    Update ProduitsSuneco.tsx

--- components/ProduitsSuneco.tsx
+++ components/ProduitsSuneco.tsx
@@ -43,6 +43,15 @@ export default function ProduitsSuneco() {
           </div>
         </div>
       </div>
+      <div>
+        <div className="text-center px-7 flex flex-col gap-5 sm:items-center">
+          <h2 className="text-2xl font-bold sm:max-w-[600px]">
+            Une sélection de Micro-Onduleur(s) solaires fiables rigoureusement choisies par nos experts en énergie
+          </h2>
+          <p className="sm:max-w-[600px]">
+            Une centrale solaire avec une solution de pilotage de la production, ainsi qu&apos;une application de suivi de consommation.
+          </p>
+        </div>
         <div className="flex flex-col lg:flex-row lg:justify-center lg:items-center mt-12 lg:mt-20 gap-10 lg:gap-12 xl:gap-24 2xl:gap-32 xl:h-auto">
           <Image
             src={"/assets/IQ8P_image.png"}
