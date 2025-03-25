c382f42eb06b94e9491bf08255af1aa863ac9c92
Max17800 <156446025+Max17800@users.noreply.github.com>
2025-03-25 15:02:43 +0100

    Update SunlineExpertSolaire.tsx

--- components/Home/SunlineExpertSolaire.tsx
+++ components/Home/SunlineExpertSolaire.tsx
@@ -23,7 +23,7 @@ export default function SunlineExpertSolaire() {
             <p className="px-8 sm:px-0 sm:max-w-[600px] xl:max-w-[500px] mt-10">
               Sunline est une entreprise de référence dans le milieu de solaire
               et dans les énergies renouvelables. 
-              Sunline s’est donné pour objectif de rendre le solaire accessible à tous, en offrant le
+              Sunline s&apos;est donné pour objectif de rendre le solaire accessible à tous, en offrant le
               meilleur compromis entre prix, rentabilité et performance, nous mettons à votre disposition tout le savoir faire de nos techniciens experts du photovoltaïque.
             </p>
           </div>
